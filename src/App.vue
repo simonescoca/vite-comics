4a67c8a23bc9e41e570b1f1e464965ff35d0cebf
simonescoca <simone.scoca@icloud.com>
2023-06-15 16:50:04 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template lang="">
+    <div>
+        <AppHeader />
+        <AppMain />
+        <AppFooter />
+    </div>
+</template>
+
+<script>
+    // Descrizione:
+    // Create un nuovo progetto utilizzando Vite e Vue 3 e definite i componenti necessari per strutturare il layout come da screenshot allegato.
+    // Quando la struttura a macroblocchi è pronta, popolate le voci di menu dinamicamente usando i data del componente.
+    // Per oggi diamo priorità alla struttura: quando è tutto bello solido, passiamo al Sass!
+    // Bonus:
+    // Creare un componente aggiuntivo per gestire la fascia azzurra con le icone.
+    import AppHeader from "./components/AppHeader.vue"
+    import AppMain from "./components/AppMain.vue"
+    import AppFooter from "./components/AppFooter.vue"
+    components: {
+        AppHeader,
+        AppMain,
+        AppFooter
+    }
+    export default {
+        data() {
+            return {
+                
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import "./styles/general.scss";
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+    @use  "../styles/partials/variables";
+    @use "../styles/partials/mixins";
+</style>
