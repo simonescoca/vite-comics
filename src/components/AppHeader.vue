28aa9b9796e55199bf68f212bcf70b60aac4feeb
simonescoca <simone.scoca@icloud.com>
2023-06-15 17:28:51 +0200

    added header base structure

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,14 +1,30 @@
 <template lang="">
-    <div>
-        
-    </div>
+    <header class="my_header">
+        <section class="my_logo-sect">
+            <img :src="logoIMG.src" :alt="logoIMG.alt">
+        </section>
+        <section class="my_links-sect">
+            <ul>
+                <li v-for="(element, index) in navLinks">
+                    <a href="#">
+                        {{navLinks[index]}}
+                    </a>
+                </li>
+            </ul>
+        </section>
+    </header>
 </template>
 
 <script>
     export default {
+        name: 'AppHeader',
         data() {
             return {
-                
+                navLinks: ["CHARACTERS", "COMICS", "MOVIES", "TV", "GAMES", "COLLECTIBLES", "VIDEOS", "FANS", "NEWS", "SHOP"],
+                logoIMG: {
+                    src: "../assets/img/dc-logo.png",
+                    alt: "DC Logo"
+                }
             }
         },
     }
@@ -17,4 +33,17 @@
 <style lang="scss" scoped>
     @use  "../styles/partials/variables";
     @use "../styles/partials/mixins";
+
+    .my_header {
+        display: flex;
+        align-items: center;
+
+        .my_logo-sect {
+
+        }
+
+        .my_links-sect {
+
+        }
+    }
 </style>
