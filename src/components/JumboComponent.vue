4a13a8a38d23ae41346aeafc465a4a3c92293c8d
simonescoca <simone.scoca@icloud.com>
2023-06-16 18:53:01 +0200

    added Jumbo component

---
+++ src/components/JumboComponent.vue
@@ -0,0 +1,46 @@
+<template lang="">
+    <div class="my_jumbo-background">
+        <!-- background -->
+        <div class="my_current-series">
+            CURRENT SERIES
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                
+            }
+        },
+
+        props: {
+
+        },
+
+        components: {
+
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .my_jumbo-background {
+        background-image: url("../assets/img/jumbotron.jpg");
+        background-position: top;
+        background-size: cover;
+        height: 500px;
+        position: relative;
+    }
+
+    .my_current-series {
+        background-color: #0182f9;
+        padding: .5rem 1rem;
+        position: absolute;
+        bottom: -15px;
+        left: 15%;
+        color: white;
+        font-weight: 700;
+    }
+</style>
