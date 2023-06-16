2e0ad43f79ed61f5beaaec0c101b8387218282ff
simonescoca <simone.scoca@icloud.com>
2023-06-16 18:52:03 +0200

    added bettre comments to today's deliver

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,16 @@
 </template>
 
 <script>
-    // Gio 15 Giu 2023
-    // Descrizione:
+    // * Gio 15 Giu 2023
+    // # Descrizione:
     // Create un nuovo progetto utilizzando Vite e Vue 3 e definite i componenti necessari per strutturare il layout come da screenshot allegato.
     // Quando la struttura a macroblocchi è pronta, popolate le voci di menu dinamicamente usando i data del componente.
     // Per oggi diamo priorità alla struttura: quando è tutto bello solido, passiamo al Sass!
     // Bonus:
     // Creare un componente aggiuntivo per gestire la fascia azzurra con le icone.
 
-    // Ven 16 Giu 2023
-    // Descrizione:
+    // * Ven 16 Giu 2023
+    // # Descrizione:
     // Continuate a lavorare nella stessa repo di ieri e create un nuovo componente che rappresenterà le card dei fumetti.
     // Utilizzate i dati presenti nel file json che trovate in allegato e passateli al componente Card tramite props.
     // Una volta inseriti tutti i contenuti dinamicamente, completate il vostro layout e rifinite i dettagli con Sass.
