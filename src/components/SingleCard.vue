b28a45b1bd17d62de01800e71ed42dcd29c230b5
simonescoca <simone.scoca@icloud.com>
2023-06-16 18:53:28 +0200

    added single-card component

---
+++ src/components/SingleCard.vue
@@ -0,0 +1,49 @@
+<template lang="">
+    <div class="card">
+        <img :src="propsCard.thumb" class="card-img-top" :alt="propsCard.type">
+        <div class="card-body">
+            <p class="card-text">
+                {{propsCard.series}}
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                
+            }
+        },
+
+        props: {
+            propsCard: Object
+        },
+
+        components: {
+
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .card {
+        width: 200px;
+        height: 300px;
+        background-color: transparent;
+        --bs-card-border-width: 0;
+    }
+
+    .card-img-top {
+        height: 200px;
+        width: 100%;
+        object-fit: cover;
+        object-position: top;
+    }
+
+    .card-text {
+        color: white;
+        text-transform: uppercase;
+    }
+</style>
